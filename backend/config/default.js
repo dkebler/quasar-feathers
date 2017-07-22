efa19110a5c9a77c5da84f0db7c3f636d1fc6e28
david kebler <d@kebler.net>
2017-07-22 17:11:05 +0000

    initial commit - sepearted into front/backend dirs

---
+++ backend/config/default.js
@@ -0,0 +1,46 @@
+// change this path if moving backend relative to client frontend
+var clientConfig = require('../../frontend/config')
+
+module.exports = {
+  client: clientConfig,
+
+  // Proxy your API if using any.
+  // Also see /build/script.dev.js and search for "proxy api requests"
+  // https://github.com/chimurai/http-proxy-middleware
+  proxyTable: {},
+
+  port: process.env.PORT || 8081,
+
+  apiPath: '/api',
+
+  host: 'localhost',
+  paginate: {
+    default: 10,
+    max: 50
+  },
+  authentication: {
+    secret: 'b5KqXTye4fVxhGFpwMVZRO3R56wS5LNoJHifwgGOFkB5GfMWvIdrWyQxEJXswhAC',
+    strategies: [
+      'jwt',
+      'local'
+    ],
+    path: '/authentication',
+    service: 'users'
+  },
+  nedb: '../data'
+}
+
+/*
+ * proxyTable example:
+ *
+   proxyTable: {
+      // proxy all requests starting with /api
+      '/api': {
+        target: 'https://some.address.com/api',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
+    }
+ */
